ff47831eddb7eb095185547907bed0b539c36087
JVitor22 <jvfaria015@gmail.com>
2019-12-06 17:43:01 -0300

    Testes

--- testeconn.php
+++ testeconn.php
@@ -4,6 +4,19 @@ use Source\Control\ControlCliente;
 
 require __DIR__.'/vendor/autoload.php';
 
+$threeMonthsAgo = strtotime('-3 Months');
+
+var_dump(date('Y-d-m' , $threeMonthsAgo));
+
+
+
+
+$sql = "INSERT INTO clientes(clientes.nome,clientes.endereco,clientes.cidade,clientes.email,
+clientes.tipo,clientes.telefone) VALUES ('nome' , 'endereco', 'cidade', 'email', 'J', 'telefone')";
+$query = Connection::prepare($sql);
+
+$query->execute(); 
+if(isset($query)) echo "OK";
+
 $c = new ControlCliente();
-$sql = $c->findAll();
-print_r(json_encode($sql));
+echo "NUMERO CLIENTES" . $c->numberClients();
